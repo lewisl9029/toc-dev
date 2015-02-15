89a78ef360b30d09c65de689e6889e2ec81a15dc
Lewis Liu <develop@lewisl.net>
2015-02-14 19:09:38 -0500

    Updated drone setup prep steps

--- drone-provision.sh
+++ drone-provision.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-#manually setup git and clone repo
+#manually setup git and clone repo, see drone.sh in secrets
 #echo GITHUB_OAUTH_TOKEN= | sudo tee -a /etc/environment
 #echo BITBUCKET_OAUTH_TOKEN= | sudo tee -a /etc/environment
 #echo DRONE_GITHUB_CLIENT= | sudo tee -a /etc/environment
@@ -9,18 +9,20 @@
 #echo DRONE_SMTP_FROM= | sudo tee -a /etc/environment
 #echo DRONE_SMTP_USER= | sudo tee -a /etc/environment
 #echo DRONE_SMTP_PASS= | sudo tee -a /etc/environment
-#exit
-#vagrant ssh
 #sudo apt-get update \
 #  && sudo apt-get install -y \
 #    git=1:1.9.1-1ubuntu0.1 \
 #  && sudo apt-get clean
 #cd ~
+#GITHUB_OAUTH_TOKEN=
 #git clone https://$GITHUB_OAUTH_TOKEN@github.com/lewisl9029/toc.git
+#git clone https://$GITHUB_OAUTH_TOKEN@github.com/lewisl9029/toc-env.git
+#
+#cd toc-env
+#
+#source drone-provision.sh
 #deprovision and create vm image
-#source ./drone-provision.sh
 
 source ./vagrant-provision.sh
 
 source ./toc-setup-drone.sh
-# source ./containers/toc-setup-build.sh
